cff78500ab6b4f0279e4f24d0e97e3cc3bc25f9f
Maruf Hossain <maruf.bshs@gmail.com>
2025-11-15 00:54:30 +0600

    Add Paper and UserUpload models with serializers and views; update settings

--- backend/paper/models.py
+++ backend/paper/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Paper(models.Model):
+    paper_id = models.CharField(max_length=50, unique=True)  # arXiv ID 
+    title = models.CharField(max_length=500)
+    authors = models.TextField()
+    abstract = models.TextField()
+    categories = models.CharField(max_length=200, null=True, blank=True)
+    journal_ref = models.CharField(max_length=255, null=True, blank=True)
+    doi = models.CharField(max_length=255, null=True, blank=True)
+    publication_year = models.IntegerField(null=True, blank=True)
+    embedding = models.JSONField(null=True, blank=True)   # vector representation
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class UserUpload(models.Model):
+    upload_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=500, blank=True, null=True)
+    authors = models.TextField(blank=True, null=True)
+    abstract = models.TextField()
+    categories = models.CharField(max_length=200, blank=True, null=True)
+    embedding = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title if self.title else f"User Upload {self.upload_id}"
---
+++ backend/paper/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers # type: ignore
+from .models import *
+
+class PaperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paper
+        fields = '__all__'  
+        read_only_fields = ['id', 'created_at']
+
+class UserUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserUpload
+        fields = '__all__'  
+        read_only_fields = ['id', 'created_at']
