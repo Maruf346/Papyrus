96dcd0fb6b29bdb32f89bd523de2a84ae3391ea5
Maruf Hossain <maruf.bshs@gmail.com>
2025-11-15 00:54:30 +0600

    Add Paper and UserUpload models with serializers and views; update settings

--- backend/paper/views.py
+++ backend/paper/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets # type: ignore
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class PaperViewSet(viewsets.ModelViewSet):
+    queryset = Paper.objects.all().order_by('-created_at')
+    serializer_class = PaperSerializer
+    search_fields = ['title', 'authors', 'abstract', 'categories', 'journal_ref']
+    ordering_fields = ['publication_year', 'created_at']
+
+class UserUploadViewSet(viewsets.ModelViewSet):
+    queryset = UserUpload.objects.all().order_by('-created_at')
+    serializer_class = UserUploadSerializer
+    search_fields = ['title', 'authors', 'abstract', 'categories']
+    ordering_fields = ['created_at']
