eca308aafb330ef1f5c4f2804d713d1f3355c3de
Maruf Hossain <maruf.bshs@gmail.com>
2025-12-08 18:48:43 +0600

    Implement code changes to enhance functionality and improve performance

--- frontend/src/components/FeaturesGrid.jsx
+++ frontend/src/components/FeaturesGrid.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const FeaturesGrid = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <section className="bg-white-500 rounded-2xl p-8">
+        <div>
+          <p className="flex items-center justify-center text-stone-600 font-semibold">Academics and Professionals from Top Institutions use Paperguide for Research</p>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="max-w-81 h-auto bg-white flex items-center justify-center">
+            <img alt="Who trusts us" loading="lazy" width="3120" height="216" decoding="async" data-nimg="1" className="object-contain grayscale hidden sm:block" style={{ color: "transparent" }} src="/brands_image.png" />
+          </div>
+        </div>
+    </section>
   )
 }
 
