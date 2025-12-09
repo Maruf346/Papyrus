4f35841ee0e16447409c1cd731c78128d3e78f32
Maruf Hossain <maruf.bshs@gmail.com>
2025-12-09 13:46:56 +0600

    Update static file settings for improved organization and production readiness

--- backend/papyrus/settings.py
+++ backend/papyrus/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # for production collectstatic
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -134,7 +137,7 @@ JAZZMIN_SETTINGS = {
     "site_title": "Papyrus Admin",
     "site_header": "Papyrus",
     "site_brand": "Papyrus",
-    "site_logo": "/static/images/logo.png",  # optional, we can skip for now
+    "site_logo": "images/logo.png",  # optional, we can skip for now
     "welcome_sign": "Welcome to the Papyrus Admin Panel",
     "copyright": "Papyrus",
 
