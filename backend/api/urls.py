d278326e6d1fb8b9cac6669277f1718a9344e34d
Maruf Hossain <maruf.bshs@gmail.com>
2025-12-13 00:38:38 +0600

    Add drf_spectacular for OpenAPI schema generation and configure REST framework settings

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,36 @@
 from rest_framework.routers import DefaultRouter # type: ignore
 from paper.views import *
 from django.urls import path, include
-
-
+from drf_spectacular.views import ( # type: ignore
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 router = DefaultRouter()
+
 router.register(r'papers', PaperViewSet, basename='paper')
 router.register(r'user-uploads', UserUploadViewSet, basename='userupload')
 
+"""API URL Configuration
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Papyrus API",
+        default_version='v1',
+        description="API documentation for Papyrus",
+         terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="maruf.bshs@gmail.com"),
+      license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+"""
 urlpatterns = [
-    path('', include(router.urls)),        
+    path('', include(router.urls)),    
+    
+    # Swagger UI
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
+
+
+
--- backend/papyrus/settings.py
+++ backend/papyrus/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'paper',
     'api',
     'rest_framework',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',  # optional, for Swagger UI assets
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +87,29 @@ DATABASES = {
     }
 }
 
+
+# Rest Framework Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    # "DEFAULT_AUTHENTICATION_CLASSES": (
+    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
+    # ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 15,
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ),
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
