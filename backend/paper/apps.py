d85061bb755d9eacc6f1e6942e4b101cececbca1
Maruf Hossain <maruf.bshs@gmail.com>
2025-12-19 10:51:35 +0600

    added __init__.py and modified app for properly configuring FAISS

--- backend/paper/apps.py
+++ backend/paper/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PaperConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'paper'
+
+    def ready(self):
+        from paper.utils.faiss_index import build_faiss_index
+        build_faiss_index()
